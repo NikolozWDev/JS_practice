cbee194964c8172533b82299acb37e258ba11d52
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-01-21 00:09:02 +0400

    rock, paper, scessiors logic

--- JSFullCourse/JSFullCourse.js
+++ JSFullCourse/JSFullCourse.js
@@ -1,5 +1,14 @@
-alert("amazon.com");
+// alert("amazon.com");
 
+// for testing (add cart)
 let variable1 = 0;
 
+
+// for project cartQuantity
 let cartQuantity = 0;
+
+
+//for project rock, paper, scissors
+const randomNumber = Math.random();
+let computerMove = ''
+let result = ''
