846e973b5b231028ea95e30ef9b1e6d5557d196d
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-01-24 00:04:18 +0400

    practicing excersises

--- practicing.js
+++ practicing.js
@@ -14,3 +14,29 @@ console.log(`you have to pay ${costResult}$`);
 let operationSo1 = Math.random() * 100;
 let ageF1 = Math.round(operationSo1);
 console.log(ageF1);
+
+
+
+function practicingF1 (nameF = 'Hi there') {
+    console.log(`Hello ${nameF}`);
+};
+practicingF1('Nikoloz');
+practicingF1('Nikoloz');
+practicingF1('Nikoloz');
+
+
+
+function celiusBy(celsius) {
+    return (celsius * 9 / 5) + 32;
+};
+console.log(celiusBy(25));
+
+
+
+let Km;
+let Ft;
+function calculateLength(Ml, Ft) {
+    Km = (16 * Ml) / 10;
+    Ft = 5280 * Ml;
+};
+console.log(calculateLength(30, 20));
