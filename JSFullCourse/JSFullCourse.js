d1d0a381369882ae6a3c7097ec42e654dfd8eed7
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-01-23 12:11:03 +0400

    fixed rock paper scissors problem

--- JSFullCourse/JSFullCourse.js
+++ JSFullCourse/JSFullCourse.js
@@ -9,6 +9,40 @@ let cartQuantity = 0;
 
 
 //for project rock, paper, scissors
-const randomNumber = Math.random();
-let computerMove = ''
-let result = ''
+function finalGame(playerMove) {
+
+    const randomNumber = Math.random();
+    let computerMove = '';
+    let result = '';
+
+
+    if(randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = 'Rock';
+    } else if(randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = 'Paper';
+    } else if(randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove = 'Scissors';
+    };
+
+    rockArgue = 'Rock';
+    paperArgue = 'Paper';
+    scissorsArgue = 'Scissors';
+
+
+    if(playerMove === computerMove) {
+        result = 'Tie'
+    } else if(
+        (playerMove === 'Rock' && computerMove === 'Scissors') ||
+        (playerMove === 'Paper' && computerMove === 'Rock') ||
+        (playerMove === 'Scissors' && computerMove === 'Paper')
+    ) {
+        result = 'You win';
+    } else {
+        result = 'You lose';
+    };
+
+
+    alert(`${playerMove}. computer move: ${computerMove}. ${result}`);
+
+
+}
