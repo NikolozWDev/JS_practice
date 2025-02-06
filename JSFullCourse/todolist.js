6c39351493851675b8fef3dc7368ac6746b5f42f
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-02-07 00:02:22 +0400

    written todolist logics

--- JSFullCourse/todolist.js
+++ JSFullCourse/todolist.js
@@ -3,17 +3,24 @@
 
 let todoListArray = [];
 
-let htmlShow = '';
-
 
 function todoListpar() {
+
+    let htmlShow = '';
+
 
     i30 = 0;
     while(i30 < todoListArray.length) {
 
         let todopro = todoListArray[i30];
-
-        let htmlparagraph = `<p>${todopro}</p>`
+        let htmlparagraph = `<div class="diviable">
+        <div class="widthpro">${todopro.name}</div> 
+        <div>${todopro.date}</div> 
+        <button class="deletebutton" onclick="
+        todoListArray.splice(${i30}, 1);
+        todoListpar();
+        ">Delete</button>
+        </div>`
 
 
         htmlShow = htmlShow + htmlparagraph;
@@ -21,20 +28,44 @@ function todoListpar() {
         i30 = i30 + 1;
     };
 
+
+    document.querySelector('#listparagraph').innerHTML = htmlShow;
+
 };
 
-document.querySelector('#listparagraph').innerHTML = htmlShow;
 
 
 function todoListF3() {
 
+    let todoArrayValuemini = document.querySelector('#todoid');
     let todoListValue = document.querySelector('#todoid').value;
 
-    todoListArray.push(todoListValue);
+    let dateValuemini = document.querySelector('#dateid');
+    let dateValue = document.querySelector('#dateid').value;
 
-    todoListValue = '';
 
-    console.log(todoListArray);
+    if(todoListValue === '' && dateValue === '') {
+        document.querySelector("#hiddenwarn").innerHTML = `Please Submit TodoName`;
+        document.querySelector("#hiddenwarn2").innerHTML = `Please Submit Date`;
+    } else if(todoListValue === '') {
+        document.querySelector("#hiddenwarn").innerHTML = `Please Submit TodoName`;
+        document.querySelector("#hiddenwarn2").innerHTML = ``;
+    } else if(dateValue === '') {
+        document.querySelector("#hiddenwarn").innerHTML = ``;
+        document.querySelector("#hiddenwarn2").innerHTML = `Please Submit Date`;
+    }
+     else {
+        document.querySelector("#hiddenwarn").innerHTML = ``;
+        document.querySelector("#hiddenwarn2").innerHTML = ``;
+    todoListArray.push(
+        {
+            name: todoListValue,
+            date: dateValue,
+        },
+    );
+    todoArrayValuemini.value = '';
+    dateValuemini.value = '';
+};
 
 
     todoListpar();
